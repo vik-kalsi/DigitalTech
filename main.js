2ad473f35b78775a63655c531f80d35143d6e154
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2023-11-21 19:14:58 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+//Fading in Opacity to 1
+$( document ).ready(function() {
+  $("#MainFrontPage").fadeTo(2000, 1);
+});
+
+
+$(document).on('scroll', function() {
+  if( $(this).scrollTop() >= $('#HowItWorksSection').position().top ){
+      $("#HowItWorksSection").fadeTo(1000, 1);    
+  }
+});
+
+
+$(document).on('scroll', function() {
+  if( $(this).scrollTop() >= $('#AdvantagesSection').position().top ){
+      $("#AdvantagesSection").fadeTo(1000, 1);    
+  }
+});
+
+
+$(document).on('scroll', function() {
+  if( $(this).scrollTop() >= $('#FAQSection').position().top ){
+      $("#FAQSection").fadeTo(1000, 1);    
+  }
+});
+
+
 //Mobile Menu
 let menuButtonSmall = $("#menuButtonSmall");
 let menuButtonMedium = $("#menuButtonMedium");
@@ -54,21 +81,21 @@ $("#FAQMenuBtn").click(function(){
 
 //FAQ Section
 $("#ExpandToggle1").click(function(){
-    $("#FAQAnswer1").toggle();
+    $("#FAQAnswer1").toggle(300);
   });
 
   $("#ExpandToggle2").click(function(){
-    $("#FAQAnswer2").toggle();
+    $("#FAQAnswer2").toggle(300);
   });
 
   $("#ExpandToggle3").click(function(){
-    $("#FAQAnswer3").toggle();
+    $("#FAQAnswer3").toggle(300);
   });
 
   $("#ExpandToggle4").click(function(){
-    $("#FAQAnswer4").toggle();
+    $("#FAQAnswer4").toggle(300);
   });
 
   $("#ExpandToggle5").click(function(){
-    $("#FAQAnswer5").toggle();
+    $("#FAQAnswer5").toggle(300);
   });
